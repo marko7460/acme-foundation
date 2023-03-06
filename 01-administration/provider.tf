04e0f44b3dc42ad80f301775e13c71629de780cc
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2023-03-06 13:52:57 +0530

    Update provider.tf

--- 01-administration/provider.tf
+++ 01-administration/provider.tf
@@ -7,7 +7,7 @@ data "tfe_outputs" "self" {
 module "tfe_oidc" {
   # TODO: Update the source URL to point to proper version of the module
   source                             = "github.com/rohitraikar565/cloud-foundation-fabric.git//blueprints/cloud-operations/terraform-enterprise-wif/tfc-workflow-using-wif/tfc-oidc"
-  #workload_identity_pool_provider_id = var.workload_identity_pool_provider_id
+  workload_identity_pool_provider_id = var.workload_identity_pool_provider_id
   impersonate_service_account_email  = var.use_google_oath_token ? "" : data.tfe_outputs.self.0.values.service_account_self
 }
 
